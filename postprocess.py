b48c4ea124c141365fa64744dbdfe3087025378b
Nicky Nicolson <nicky.nicolson@gmail.com>
2023-10-27 15:00:25 +0100

    Focus attention on data

--- postprocess.py
+++ postprocess.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 import argparse
+import json
 
 # Simple script to convert JSON to CSV
 
@@ -11,7 +12,14 @@ if __name__ == "__main__":
     parser.add_argument("inputfile")
     args = parser.parse_args()
 
-    df = pd.read_json(args.inputfile)
+    with open(args.inputfile, 'r') as f:
+        ih_response = json.load(f)
+
+    print(ih_response['meta'])
+    df = pd.DataFrame(ih_response['data'])
+    print(df)
+    print(df.sample(n=1).T)
+
     outputfile = args.inputfile.replace('.json','.csv')
     print('{} -> {}'.format(args.inputfile, outputfile))
     df.to_csv(outputfile, index=False, sep='\t')
