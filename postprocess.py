94978ab9674ea3fd69b0ecf1bf4e11e6f6584bc8
Nicky Nicolson <nicky.nicolson@gmail.com>
2023-10-27 14:46:16 +0100

    Add python post-processing step

---
+++ postprocess.py
@@ -0,0 +1,16 @@
+import sys
+import pandas as pd
+import numpy as np
+import argparse
+
+# Simple script to convert JSON to CSV
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputfile")
+    args = parser.parse_args()
+
+    df = pd.read_json(args.inputfile)
+    outputfile = args.inputfile.replace('.json','.csv')
+    df.to_csv(outputfile, index=False, sep='\t')
